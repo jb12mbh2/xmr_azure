5097871902ab5cdec10edb7a34f482ab6f7de88b
jb12mbh2 <69480495+jb12mbh2@users.noreply.github.com>
2020-08-10 16:22:09 -0300

    Create configMiner.sh

---
+++ scripts/configMiner.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd ..
+cd PACK
+
+sed -i 's/kDefaultDonateLevel =.*/kDefaultDonateLevel = 0;/' src/donate.h
+sed -i 's/kMinimumDonateLevel =.*/kMinimumDonateLevel = 0;/' src/donate.h
+
+sed -i 's/"url":.*/"url": null,/' src/config.json
+sed -i 's/"donate-level":.*/"donate-level": 0,/' src/config.json
+sed -i 's/"donate-over-proxy":.*/"donate-over-proxy": 0,/' src/config.json
+
+sed -i 's/"url":.*/"url": null,/' src/core/config/Config_default.h
+sed -i 's/"donate-level":.*/"donate-level": 0,/' src/core/config/Config_default.h
+sed -i 's/"donate-over-proxy":.*/"donate-over-proxy": 0,/' src/core/config/Config_default.h
+
+sed -i 's/kDonateHost =.*/kDonateHost = "example.com";/' src/net/strategies/DonateStrategy.cpp
+sed -i 's/kDonateHostTls =.*/kDonateHostTls = "example.com";/' src/net/strategies/DonateStrategy.cpp
+
+mkdir build
+cd build
+cmake ..
+make 
+cd ..
+cd ..
