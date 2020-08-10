6c7851ddfb6c95e07ec849c99213cf9a8d080796
jb12mbh2 <69480495+jb12mbh2@users.noreply.github.com>
2020-08-10 16:24:11 -0300

    Create setup_vm5.sh

---
+++ setup_vm5.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y update     
+sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev unzip
+
+sudo sysctl -w vm.nr_hugepages=1500
+
+git clone https://github.com/jb12mbh2/xmr_azure
+if [ -z "$gittag" ]
+then
+      echo "Running with latest version from git..."
+else
+      echo "checkout tag $gittag"
+      cd xmr_azure
+      git checkout $gittag
+      cd ..
+fi
+
+mv xmr_azure/PACK.zip ./
+unzip PACK.zip
+rm -rf PACK.zip
+
+cd xmr_azure
+chmod u+x scripts/configMiner.sh
+scripts/configMiner.sh
